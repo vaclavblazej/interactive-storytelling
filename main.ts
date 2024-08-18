941c764425b84d35d24ed50a1479942bfd8d15c9
Václav Blažej <vaclavblazej@seznam.cz>
2024-08-18 23:14:40 +0100

    .

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
 import { App, Editor, FuzzySuggestModal, ItemView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SuggestModal, WorkspaceLeaf, setIcon } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface InteractiveStorytellingSettings {
     mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: InteractiveStorytellingSettings = {
     mySetting: 'default'
 }
 
@@ -16,75 +14,162 @@ function randomString(len: number) {
     return [...Array(len)].reduce(a => a+alphabet[~~(Math.random()*alphabet.length)], '');
 }
 
-function getUrlList(view: MarkdownView) {
+function getIdList(view: MarkdownView) {
     var lines = view.data.split('\n');
-    const rex: RegExp = /`url (?<url>\w+)`/g;
+    const rex: RegExp = /`id (?<id>\w+)`/g;
     var result: [string, string][] = [];
     for(var i = 0; i < lines.length; ++i){
         var line = lines[i];
         for(const match of line.matchAll(rex)) {
-            var url = match.groups?.url;
-            if(url) {
-                result.push([line, url]);
+            var id = match.groups?.id;
+            if(id) {
+                result.push([line, id]);
             }
         }
     }
     return result;
 }
 
-function parse_line(line: String): any {
+enum CommandType{
+    Id,
+    Next,
+    Choice,
+}
+
+class Command{
+    type: CommandType
+    id: number | null
+}
+
+class Entry {
+    id: number
+    next_id: number | null
+    spaces: number
+    speaker: string | null
+    choice_ids: number[] | null
+    text: string
+    commands: Command[]
+}
+
+var first_free_id: number = 0;
+
+function parse_command(command_str: string): Command{
+    const cmd_split = command_str.split(' ');
+    var res = new Command();
+    switch(cmd_split[0]){
+        case "id": {
+            res.type = CommandType.Id;
+            res.id = parseInt(cmd_split[1]);
+        }
+        case "next": {
+            res.type = CommandType.Next;
+            res.id = parseInt(cmd_split[1]);
+        }
+        case "choice": {
+            res.type = CommandType.Choice;
+            res.id = null;
+        }
+    }
+    return res;
+}
+
+// expecting lines of the following format
+// (spaces) * speaker: Text that may contain `commands` within backticks.
+// (otherwise returns null)
+function parse_line(line: string): Entry | null {
     var spaces = 0;
-    var sentence = "";
     var i = 0;
     for(; i < line.length; ++i){
         const c = line.charAt(i);
-        if(c == "*" || c == "-"){
-            spaces = i;
+        if(c == '*' || c == '-'){
             ++i;
             break;
         }
+        if(c != ' '){
+            return null;
+        }
+        spaces += 1;
     }
-    var speaker = null;
+    if(i == line.length){
+        return null;
+    }
     var code = false;
-    var cmd;
-    var commands = [];
+    var speaker = null;
+    var text = "";
+    var command: string = "";
+    var command_strings = [];
     for(; i < line.length; ++i){
         const c = line.charAt(i);
-        if(speaker == null && c == ":"){
-            speaker = sentence.trim();
-            sentence = "";
+        if(speaker == null && c == ':'){
+            speaker = text.trim();
+            if(speaker[0] == '['){
+                speaker = speaker.substring(2,speaker.length-2);
+            }
+            text = "";
         }else if(code){
             if(c == '`'){
                 code = false;
-                commands.push(cmd);
+                command_strings.push(command);
             }else{
-                cmd += c;
+                command += c;
             }
         }else{
             if(c == '`'){
                 code = true;
-                cmd = "";
+                command = "";
             }else{
-                sentence += c;
+                text += c;
             }
         }
     }
-    return {
-        spaces: spaces,
-        speaker: speaker,
-        sentence: sentence.trim(),
-        commands: commands,
-    };
+    const commands = command_strings.map(parse_command);
+    const res = new Entry();
+    for(const i in commands){
+        const command = commands[i];
+        switch(command.type){
+            case CommandType.Id: {
+            }
+            case CommandType.Next: {
+            }
+            case CommandType.Choice: {
+            }
+        }
+    }
+    if(res.id == null){
+        res.id = first_free_id;
+        first_free_id += 1;
+    }
+    res.choices = [];
+    res.next_id = null;
+    res.spaces = spaces;
+    res.speaker = speaker;
+    res.text = text.trim();
+    res.commands = commands;
+    return res;
 }
 
-function get_graph(lines: String[]): any {
-    for(const line in lines){
+function get_graph(lines: string[]): Entry[] {
+    var res = [];
+    var last = null;
+    for(var i = 0; i < lines.length; ++i){
+        const line = lines[i];
         const line_struct = parse_line(line);
+        if(line_struct){
+            line_struct.id = first_free_id;
+            if(last != null){
+                if(last.next_id == null){
+                    last.next_id = line_struct.id;
+                }
+            }
+            res.push(line_struct);
+            last = line_struct
+        }
     }
+    return res;
 }
 
 export default class MyPlugin extends Plugin {
-    settings: MyPluginSettings;
+    settings: InteractiveStorytellingSettings;
 
     async onload() {
         await this.loadSettings();
@@ -101,33 +186,34 @@ export default class MyPlugin extends Plugin {
         // statusBarItemEl.setText('Status Bar Text');
 
         this.addCommand({
-            id: 'add-url',
-            name: 'Add URL',
+            id: 'add-id',
+            name: 'Add ID',
             editorCallback: (editor: Editor, view: MarkdownView) => {
-                // console.log(editor.getSelection());
                 var new_id = randomString(4);
                 // todo check that no such id already exists
-                editor.replaceSelection('`url ' + new_id + '`');
+                editor.replaceSelection('`id ' + new_id + '`');
             }
         });
+
         // This adds a simple command that can be triggered anywhere
         this.addCommand({
-            id: 'add-goto',
-            name: 'Add Goto',
+            id: 'add-next',
+            name: 'Add Next',
             editorCallback: (editor: Editor, view: MarkdownView) => {
-                var urlList = getUrlList(view);
-                new UrlSuggestModal(this.app, editor, urlList).open();
+                var idList = getIdList(view);
+                new IdSuggestModal(this.app, editor, idList).open();
             }
         });
+
         // This adds an editor command that can perform some operation on the current editor instance
         // this.addCommand({
             // id: 'sample-editor-command',
             // name: 'Sample editor command',
             // editorCallback: (editor: Editor, view: MarkdownView) => {
-                // console.log(editor.getSelection());
                 // editor.replaceSelection('Sample Editor Command');
             // }
         // });
+
         // This adds a complex command that can check whether the current state of the app allows execution of the command
         // this.addCommand({
         // id: 'open-sample-modal-complex',
@@ -139,7 +225,7 @@ export default class MyPlugin extends Plugin {
         // // If checking is true, we're simply "checking" if the command can be run.
         // // If checking is false, then we want to actually perform the operation.
         // if (!checking) {
-        // new UrlSuggestModal(this.app).open();
+        // new IdSuggestModal(this.app).open();
         // }
 
         // // This command will only show up in Command Palette when the check function returns true
@@ -204,7 +290,7 @@ export default class MyPlugin extends Plugin {
     }
 }
 
-class UrlSuggestModal extends FuzzySuggestModal<[string, string]> {
+class IdSuggestModal extends FuzzySuggestModal<[string, string]> {
     list: [string, string][];
     editor: Editor;
 
@@ -215,8 +301,8 @@ class UrlSuggestModal extends FuzzySuggestModal<[string, string]> {
     }
 
     onChooseItem(item: [string, string], evt: MouseEvent | KeyboardEvent) {
-        var [_, url] = item;
-        this.editor.replaceSelection('`goto ' + url + '`');
+        var [_, id] = item;
+        this.editor.replaceSelection('`next ' + id + '`');
     }
 
     getItemText(item: [string, string]): string {
@@ -258,14 +344,31 @@ class SampleSettingTab extends PluginSettingTab {
 
 export const VIEW_TYPE_EXAMPLE = "example-view";
 
-var chat: any[] = [];
-var choices: any[] = [];
-var state: any = {
+var chat_history: any[] = [];
+var choices: Entry[] = [];
+var everything: any = {
+    file_start: {},
+    ids_to_structs: {},
 };
+var current_stack: number[] = [];
 
 export class ExampleView extends ItemView {
     constructor(leaf: WorkspaceLeaf) {
         super(leaf);
+    }
+
+    async push_file(filename: string) {
+        await this.app.vault.adapter.read(filename).then((file: string) => {
+            const lines = file.split('\n');
+            const entries: Entry[] = get_graph(lines);
+            for(const i in entries){
+                const entry = entries[i];
+                everything.ids_to_structs[entry.id] = entry;
+            }
+            const first_id = entries[0].id;
+            everything.file_start[filename] = first_id;
+            current_stack.push(first_id);
+        });
     }
 
     getViewType() {
@@ -277,40 +380,71 @@ export class ExampleView extends ItemView {
     }
 
     async onOpen() {
-        // container.createEl("h4", { text: "Interactive story reader" });
-        chat = [
-            { speaker: "other", text: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt." },
-            { speaker: "you", text: "Ut enim ad minim veniam, quis nostrud exercitation!" },
-            { speaker: "other", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-        ];
-        choices = [
-            { id: 1, text: "Ullamco laboris nisi ut aliquip ex ea commodo consequat, consequuntur magni dolores eos qui ratione voluptatem." },
-            { id: 2, text: "Esse cillum dolore eu fugiat nulla pariatur." },
-            { id: 3, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-        ];
-        this.render()
+        await this.start_active_file().then(() => {
+            this.render()
+        });
+    }
+
+    async start_active_file(){
+        const active_file = this.app.workspace.getActiveFile();
+        if(active_file) {
+            current_stack = [];
+            await this.push_file(active_file.name).then(() => {
+                chat_history = []
+                const last = current_stack.last();
+                if(last){
+                    chat_history.push(everything.ids_to_structs[last]);
+                }
+            });
+        }
+    }
+
+    advance_current_position(){
+        const current_id: number | undefined = current_stack.pop();
+        console.log("current_id:", current_id);
+        if(current_id == undefined){
+            console.log("undefined current_id", current_id);
+            return;
+        }
+        const current: Entry = everything.ids_to_structs[current_id];
+        if(current.next_id == null){
+            this.advance_current_position();
+            return;
+        }
+        const next_el = everything.ids_to_structs[current.next_id];
+        console.log("next:", next_el);
+        current_stack.push(next_el.id);
+        chat_history.push(next_el);
     }
 
     render(){
         const container = this.containerEl.children[1];
         container.empty();
-        var messages = container.createEl("div", { cls: "intstory_chat_window" });
-        for(const msg of chat) {
-            var cls = "intstory_message_" + msg.speaker;
-            var m = messages
+        const start_btn = container.createEl("button", { text: "Run this file", cls: "intstory_buttongs" });
+        start_btn.onClickEvent(() => {
+            this.start_active_file().then(() => {
+                this.render()
+            });
+        });
+        var messages_el = container.createEl("div", { cls: "intstory_chat_window" });
+        for(const message of chat_history) {
+            var cls = "intstory_message_" + message.speaker;
+            if(message.speaker != "You"){
+                cls += " intstory_message_other";
+            }
+            var m = messages_el
             .createEl("div", { cls: "intstory_message_border " + cls })
             .createEl("div", { cls: "intstory_message_content" });
-            m.createEl("b", { text: msg.speaker });
-            m.createEl("span", { text: " – " + msg.text, cls: "intstory_" + msg.speaker });
+            m.createEl("b", { text: message.speaker });
+            m.createEl("span", { text: " – " + message.text, cls: "intstory_" + message.speaker });
         }
-        var choices_element = messages.createEl("div", { cls: "intstory_choices" })
+        var chocies_el = messages_el.createEl("div", { cls: "intstory_choices" })
         if(choices.length != 0){
             for(const choice of choices){
-                let btn = choices_element.createEl("button", { text: "" + choice.id + " – " + choice.text, cls: "intstory_choice" })
+                const btn = chocies_el.createEl("button", { text: "" + choice.id + " – " + choice.text, cls: "intstory_choice" })
                 btn.onClickEvent(() => {
-                    console.log(choice.id);
-                    chat.push({
-                        speaker: "you",
+                    chat_history.push({
+                        speaker: "You",
                         text: choice.text,
                     });
                     choices = [];
@@ -318,9 +452,11 @@ export class ExampleView extends ItemView {
                 });
             }
         }else{
-            let btn = choices_element.createEl("button", { text: "next", cls: "intstory_next" })
+            const btn = chocies_el.createEl("button", { text: "next", cls: "intstory_next" })
             btn.onClickEvent(() => {
                 console.log("next");
+                this.advance_current_position();
+                this.render();
             });
         }
     }
